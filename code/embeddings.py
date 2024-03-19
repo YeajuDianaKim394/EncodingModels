93f9e9cd54724f56bc3f6dfc104563a3dfc09b91
Zaid Kokaja <zkokaja@gmail.com>
2024-03-19 13:39:19 -0400

    add more models

--- code/embeddings.py
+++ code/embeddings.py
@@ -2,37 +2,62 @@
 
     salloc --mem=32G --time=00:15:00 --gres=gpu:1
     python code/embeddings.py -m gpt2-xl --layer 24
-    python code/embeddings.py -m gptneo-3b --layer 16
-    python code/embeddings.py -m llama2-7b --layer 16
-    python code/embeddings.py -m llama2-7b-chat --layer 16
 """
 
 from glob import glob
+from os import makedirs
 
 import pandas as pd
 import torch
 from tqdm import tqdm
-from transformers import (
-    AutoModelForCausalLM,
-    AutoTokenizer,
-)
+from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
 from util.path import Path
 
 # short names for long model names
-EMBEDDINGS = {
+HFMODELS = {
+    "opt-125m": "facebook/opt-125m",
+    "opt-350m": "facebook/opt-350m",
+    "opt-2b": "facebook/opt-1.3b",
+    "opt-3b": "facebook/opt-2.7b",
+    "opt-7b": "facebook/opt-6.7b",
+    "opt-13b": "facebook/opt-13b",
+    "llama-7b": "models/llama/7b",
     "llama2-7b": "meta-llama/Llama-2-7b-hf",
-    "llama2-7b-chat": "meta-llama/Llama-2-7b-chat-hf",
+    # "llama2-7b-chat": "meta-llama/Llama-2-7b-chat-hf",
     "mistral-7b": "mistralai/Mistral-7B-v0.1",
-    "gptneo-3b": "EleutherAI/gpt-neo-2.7B",
+    "neo-125m": "EleutherAI/gpt-neo-125m",
+    "neo-1b": "EleutherAI/gpt-neo-1.3B",
+    "neo-3b": "EleutherAI/gpt-neo-2.7B",
+    "gptj-6b": "EleutherAI/gpt-j-6b",
+    "gpt2-82m": "distilgpt2",
+    "gpt2-124m": "gpt2",
+    "gpt2-355m": "gpt2-medium",
+    "gpt2-774m": "gpt2-large",
+    "gpt2-2b": "gpt2-xl",
 }
 
 
-def main(hfmodelname: str, device: str = "cpu", layer: int = 0):
+def get_model_metadata():
+    records = []
+    for modelname, hfmodelname in HFMODELS.items():
+        config = AutoConfig.from_pretrained(hfmodelname)
+        n_layers = config.num_hidden_layers
+        hidden_size = config.hidden_size
+        max_positions = config.max_position_embeddings
+        # params = config.num_parameters()
+        records.append((modelname, n_layers, hidden_size, max_positions))
+
+    df = pd.DataFrame(
+        records, columns=["model", "num_layers", "hidden_size", "max_positions"]
+    )
+    print(df)
+    df.to_csv("mats/models.csv", index=False)
+
+
+def main(modelname: str, device: str = "cpu", layer: int = None):
     """Reimplemented to share model across iterations and for models with big context size."""
 
-    dirname = f"model-{args.model}"
-    if args.layer != -1:
-        dirname += f"_layer-{args.layer}"
+    hfmodelname = HFMODELS[modelname]
 
     # Find transcripts
     transpath = Path(
@@ -47,13 +72,33 @@ def main(hfmodelname: str, device: str = "cpu", layer: int = 0):
     tokenizer_args = dict()
     if "gpt2" in hfmodelname:
         tokenizer_args["add_prefix_space"] = True
+    model_args = dict()
+    if "Llama-2" in hfmodelname:
+        # https://huggingface.co/docs/transformers/main/model_doc/llama2#usage-tips
+        # Setting config.pretraining_tp to a value different than 1 will
+        # activate the more accurate but slower computation of the linear
+        # layers, which should better match the original logits.
+        model_args["pretraining_tp"] = 0
+        # model_args[torch_dtype] = torch.float16
 
     # Load model
-    print("Loading model")
+    print("Loading model...")
     tokenizer = AutoTokenizer.from_pretrained(hfmodelname, **tokenizer_args)
-    model = AutoModelForCausalLM.from_pretrained(hfmodelname)
+    model = AutoModelForCausalLM.from_pretrained(hfmodelname, **model_args)
+    if layer is None:
+        layer = model.config.num_hidden_layers // 2
+
+    print(
+        f"Model : {hfmodelname}"
+        f"\nLayers: {model.config.num_hidden_layers} ({layer})"
+        f"\nEmbDim: {model.config.hidden_size}"
+        f"\nCxtLen: {model.config.max_position_embeddings}"
+    )
     model = model.eval()
     model = model.to(device)
+
+    metrics = {}
+    dirname = f"model-{modelname}_layer-{layer}"
 
     for tpath in tqdm(files):
         df = pd.read_csv(tpath)
@@ -71,15 +116,40 @@ def main(hfmodelname: str, device: str = "cpu", layer: int = 0):
 
         # Run through model
         with torch.no_grad():
-            output = model(batch, output_hidden_states=True)
+            output = model(batch, labels=batch, output_hidden_states=True)
             states = output.hidden_states[layer][0, 1:].numpy(force=True)
 
-        df["embedding"] = [e for e in states]
+            loss = output.loss
+            logits = output.logits[0]
 
+            logits_order = logits.argsort(descending=True, dim=-1)
+            ranks = torch.eq(logits_order[:-1], batch[:, 1:].T).nonzero()[:, 1]
+
+            probs = logits[:-1, :].softmax(-1)
+            true_probs = probs[0, batch[0, 1:]]
+
+            entropy = torch.distributions.Categorical(probs=probs).entropy()
+
+        df["rank"] = ranks.numpy(force=True)
+        df["true_prob"] = true_probs.numpy(force=True)
+        df["entropy"] = entropy.numpy(force=True)
+
+        metrics[tpath] = dict(
+            top1_acc=(df["rank"] == 0).mean(), perplexity=loss.exp().item()
+        )
+
+        df["embedding"] = [e for e in states]
         epath = Path.frompath(tpath)
         epath.update(root="features", datatype=dirname, suffix=None, ext="pkl")
         epath.mkdirs()
         df.to_pickle(epath)
+
+    outdir = f"features/conv/{modelname}"
+    makedirs(outdir, exist_ok=True)
+    summary_df = pd.DataFrame(metrics).T
+    summary_df.to_csv(f"{outdir}/performance.csv")
+
+    print(summary_df.describe())
 
 
 if __name__ == "__main__":
@@ -87,7 +157,7 @@ if __name__ == "__main__":
 
     parser = ArgumentParser()
     parser.add_argument("-m", "--model", default="glove-50")
-    parser.add_argument("--layer", type=int, default=-1)
+    parser.add_argument("--layer", type=int, default=None)
     parser.add_argument("--cuda", type=int, default=0)
     parser.add_argument("--force-cpu", action="store_true")
     args = parser.parse_args()
@@ -98,6 +168,5 @@ if __name__ == "__main__":
     else:
         print("WARNING: using cpu only")
 
-    args.hfmodelname = EMBEDDINGS.get(args.model, args.model)
-
-    main(args.hfmodelname, device=args.device, layer=args.layer)
+    main(args.model, device=args.device, layer=args.layer)
+    # get_model_metadata()
